0a66808972b6ab947948ad6a3c18f1daf55d515c
zunaizah <zunaizah314@gmail.com>
2025-11-27 13:29:13 +0700

    me-render component Skills.jsx ke Apps.jsx

--- cv-aku/src/App.jsx
+++ cv-aku/src/App.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
 import Profile from './components/Profile';
-
+import Skills from './components/Skills';
 
 function App() {
   return (
     <div className="container" style={{ paddingTop: '2rem', paddingBottom: '4rem' }}>
       <Profile />
-
+      <Skills />
     </div>
   );
 }
