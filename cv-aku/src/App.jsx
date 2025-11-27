3a0090c1f5d6f592086b4a8c106d58b708e8be46
zunaizah <zunaizah314@gmail.com>
2025-11-27 13:48:04 +0700

    me-render component Experiences.jsx ke Apps.jsx

--- cv-aku/src/App.jsx
+++ cv-aku/src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './components/components.css';
 import Profile from './components/Profile';
 import Skills from './components/Skills';
+import Experiences from './components/Experiences';
 
 function App() {
   return (
     <div className="container" style={{ paddingTop: '2rem', paddingBottom: '4rem' }}>
       <Profile />
       <Skills />
+      <Experiences />
     </div>
   );
 }
